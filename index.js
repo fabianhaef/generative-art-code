72886c1fa582a1e4503c97e48143c57090298e70
Fabian Häfliger <fabian.haefliger@students.fhnw.ch>
2021-10-26 22:34:43 +0200

    refactoring layer and save function

--- index.js
+++ index.js
@@ -4,29 +4,39 @@ const fs = require("fs")
 // load functions form canvas library
 const { createCanvas, loadImage} = require("canvas");
 
+// import layers, width and height from config
+const {layers, width, height} = require('./input/config.js');
+
 // create a canvas with 1000 x 1000 Pixel size
 const canvas = createCanvas(1000, 1000);
 
 // context is mandatory to draw and make shapes etc.
 const ctx = canvas.getContext("2d")
 
+// Number of Images you want to create
+const edition = 10;
 
-const saveLayer = (_canvas) => {
+const saveLayer = (_canvas, _edition) => {
   // to save file you need to buffer the image wiht the to buffer function
-  fs.writeFileSync("./newImage.png", _canvas.toBuffer("image/png"));
-  console.log("Image created")
-
+  fs.writeFileSync(`./output/${_edition}.png`, _canvas.toBuffer("image/png"));
 };
 
 // multiple layers for our art => not only one layer
-const drawLayer = async () => {
+const drawLayer = async (_layer, _edition) => {
+  let element = _layer.elements[Math.floor(Math.random() * _layer.elements.length)]
+  console.log("Element", element)
   // takes in data as parameter after image is loaded
-  const image = await loadImage('./Logo.png')
+  const image = await loadImage(`${_layer.location}${element.fileName}`)
   // parameters are img, x, y, width and height
-  ctx.drawImage(image, 0, 0, 1000, 1000);
-
-  console.log("This ran");
-  saveLayer(canvas);
+  ctx.drawImage(image, _layer.position.x, _layer.position.y, _layer.position.width, _layer.position.height);
+  console.log(`created layer ${_layer.name} and choose element ${element.name}`)
+  saveLayer(canvas, _edition);
 };
 
-drawLayer();
+for(let i = 1; i <= edition; i++) {
+  layers.forEach((layer) => {
+    drawLayer(layer, i);
+  });
+
+  console.log("Creating edition" + i)
+}
